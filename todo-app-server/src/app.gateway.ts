bc8dd465adfbf30deb2216181fcf78582b0ec641
dvirdaniel <dvirdaniel@gmail.com>
2019-12-06 12:47:19 +0200

    Update app.gateway.ts

--- todo-app-server/src/app.gateway.ts
+++ todo-app-server/src/app.gateway.ts
@@ -9,7 +9,7 @@ import {
 import {Logger} from '@nestjs/common';
 import {Server, Socket} from "socket.io";
 
-@WebSocketGateway( {'transports': ['websocket', 'polling']} )
+@WebSocketGateway(3000, {'transports': ['websocket', 'polling']} )
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 
   private logger: Logger = new Logger('AppGateway');
