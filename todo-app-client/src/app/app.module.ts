b492617d63d63c8d30434006ead9eaaed238191a
dvirdaniel <dvirdaniel@gmail.com>
2019-12-07 13:31:52 +0200

    Update app.module.ts

--- todo-app-client/src/app/app.module.ts
+++ todo-app-client/src/app/app.module.ts
@@ -11,7 +11,7 @@ import {TasksComponent} from "./tasks/tasks.component";
 import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
 
 const config: SocketIoConfig = {
-  url: '/websocket', options: { 'transports': ['websocket', 'polling'] }
+  url: '', options: { 'transports': ['websocket', 'polling'] }
 };
 
 @NgModule({
